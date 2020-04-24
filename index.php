707f3b1c7da61b4bc6692af64be3e09d64e79e1d
4gruppam <64256555+4gruppam@users.noreply.github.com>
2020-04-24 15:15:53 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<?php
+// Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}} 
 
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClickClient('https://flo-market.ru/api.php?', 'pPH7JntghwSrfcwQ');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
 <!DOCTYPE html>
-<html data-scrapbook-source="http://healty6.caallm.org/" data-scrapbook-create="20200331070421242">
+<html >
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
